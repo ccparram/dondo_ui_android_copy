5968fbb70b89b83aaa21d6c14d3ba8237b6eddd4
Camilo Parra <camiloparradev@gmail.com>
2022-01-28 20:05:12 -0500

    Update ExoPlayer

--- ui/src/main/java/com/dondo/ui/mediaslider/VideoViewHolder.kt
+++ ui/src/main/java/com/dondo/ui/mediaslider/VideoViewHolder.kt
@@ -3,33 +3,20 @@ package com.dondo.ui.mediaslider
 import android.net.Uri
 import androidx.recyclerview.widget.RecyclerView
 import com.dondo.ui.databinding.ElementVideoBinding
-import com.google.android.exoplayer2.DefaultLoadControl
-import com.google.android.exoplayer2.DefaultRenderersFactory
-import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 
 class VideoViewHolder(private val binding: ElementVideoBinding) : RecyclerView.ViewHolder(binding.root) {
 
 	fun bind(url: String) {
 		binding.run {
 
-			val player = ExoPlayerFactory.newSimpleInstance(
-				DefaultRenderersFactory(binding.root.context),
-				DefaultTrackSelector(), DefaultLoadControl()
-			)
-
-			val mediaUri = Uri.parse(url)
-
-			val mediaSource = ExtractorMediaSource
-				.Factory(DefaultHttpDataSourceFactory("media-slider-view"))
-				.createMediaSource(mediaUri)
+			val player = ExoPlayer.Builder(binding.root.context).build()
+			val mediaItem = MediaItem.fromUri(Uri.parse(url))
 
 			playerView.player = player
-			player.prepare(mediaSource, true, true)
-			player.playWhenReady = false
-			player.seekTo(0, 0)
+			player.setMediaItem(mediaItem)
+			player.prepare()
 		}
 	}
 }
