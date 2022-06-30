b7ea9f039b1f1d59707d618103faf402138ae00f
Alejandro Rios <alejandrorios77@gmail.com>
2022-06-30 11:17:18 -0500

    - EditTextField Clickable property implemented.

--- app/src/main/java/com/dondo/demo/examples/TextFieldsActivity.kt
+++ app/src/main/java/com/dondo/demo/examples/TextFieldsActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.text.InputType.TYPE_CLASS_TEXT
 import android.text.InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
 import android.text.InputType.TYPE_TEXT_FLAG_MULTI_LINE
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
 import com.dondo.demo.databinding.ActivityTextFieldsBinding
 import com.dondo.ui.utils.extensions.viewBinding
@@ -18,8 +20,13 @@ class TextFieldsActivity : AppCompatActivity() {
 
         with(binding) {
             etMultiline.inputType = TYPE_CLASS_TEXT or TYPE_TEXT_FLAG_CAP_SENTENCES or TYPE_TEXT_FLAG_MULTI_LINE
+
             etDisable.text = "This is a disabled field"
             etDisable.isEnabled = false
+
+            etClickable.setOnClickListener {
+                Toast.makeText(this@TextFieldsActivity, "TextField Clicked!", LENGTH_SHORT).show()
+            }
         }
     }
 }
